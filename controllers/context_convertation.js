c6eed7c6f868d8e82a1b4d8f6ffd783a0384de5a
mohamedbougarn <mohamedbougarn@gmail.com>
2022-03-31 10:44:50 +0100

    update add

--- controllers/context_convertation.js
+++ controllers/context_convertation.js
@@ -24,15 +24,16 @@ const Context_conversationGet =(request, response) => {
 
 };
 
-
+//execute after the tretement.js add responce  
 const Context_conversationAdd =(request, response) => {
 
     p_id_context = request.body.id_context;
     p_question = request.body.question;
+    p_response = request.body.response;
 
-    db.sequelize.query('SELECT * FROM ctl_context_convertation_add(:id_context , :question) ',
+    db.sequelize.query('SELECT * FROM ctl_context_convertation_add(:id_context , :question, :response) ',
 
-        { replacements: {id_context:p_id_context,question:p_question}, type: db.sequelize.QueryTypes.SELECT },
+        { replacements: {id_context:p_id_context,question:p_question,response:p_response}, type: db.sequelize.QueryTypes.SELECT },
         {
             model: context_conversation,
             mapToModel: true // pass true here if you have any mapped fields
@@ -47,6 +48,27 @@ const Context_conversationAdd =(request, response) => {
 
 };
 
+// const Context_conversationAdd =(request, response) => {
+
+//     p_id_context = request.body.id_context;
+//     p_question = request.body.question;
+
+//     db.sequelize.query('SELECT * FROM ctl_context_convertation_add(:id_context , :question) ',
+
+//         { replacements: {id_context:p_id_context,question:p_question}, type: db.sequelize.QueryTypes.SELECT },
+//         {
+//             model: context_conversation,
+//             mapToModel: true // pass true here if you have any mapped fields
+//         }).then(context_conversation => {
+//         logger.info(context_conversation)
+//         response.json(context_conversation)
+//     }).catch(err => {
+
+//         logger.error(err)
+//         response.status(500).json({msg: "error", details: err});
+//     });
+
+// };
 
 const Context_conversationUpload =(request, response) => {
 
