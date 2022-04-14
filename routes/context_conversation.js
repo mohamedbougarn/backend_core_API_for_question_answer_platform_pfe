3f59654c8fb10194b1bea53595afdfacdd59d63d
mohamedbougarn <mohamedbougarn@gmail.com>
2022-04-14 14:18:00 +0100

    add root

--- routes/context_conversation.js
+++ routes/context_conversation.js
@@ -5,6 +5,7 @@ module.exports = function(app) {
     app.put('/Context_conversation/get',contextconversation.Context_conversationGet);
     app.put('/Context_conversation/add', contextconversation.Context_conversationAdd);
     app.put('/context_conversation/upload', contextconversation.Context_conversationUpload);
+    app.put('/context_conversation/count', contextconversation.Context_ConversationCount);
 
 
 }
