6f85872b0253cf065cb1e0d69297807925c96c85
mohamedbougarn <mohamedbougarn@gmail.com>
2022-04-21 12:48:45 +0100

    add visiteur controller methods

--- controllers/visiteur.js
+++ controllers/visiteur.js
@@ -26,6 +26,29 @@ const Visiteur_Context_conversationGet = (req, res)=>
 
 
 //execute after the tretement.js add responce  
+const Visiteur_Context_conversationAdd =(request, response) => {
+
+    p_id_context = request.body.id_context;
+    p_question = request.body.question;
+    p_response = request.body.response;
+
+    db.sequelize.query('SELECT * FROM ctl_visiteur_context_convertation_add(:id_context , :question, :response) ',
+
+        { replacements: {id_context:p_id_context,question:p_question,response:p_response}, type: db.sequelize.QueryTypes.SELECT },
+        {
+            model: visiteur_conversation,
+            mapToModel: true // pass true here if you have any mapped fields
+        }).then(visiteur_conversation => {
+        logger.info(visiteur_conversation)
+        response.json(visiteur_conversation)
+    }).catch(err => {
+
+        logger.error(err)
+        response.status(500).json({msg: "error", details: err});
+    });
+
+};
+
 
 
 
