e8b8cfadbf183b8477e66d55e5d112c175868557
mohamedbougarn <mohamedbougarn@gmail.com>
2022-04-25 23:57:21 +0100

    route & controller core for graph statisrique

--- routes/stat.js
+++ routes/stat.js
@@ -2,6 +2,7 @@ module.exports = function(app) {
     const stat = require('../controllers/stat'); 
 
     app.post('/stat/msg_date', stat.Stat_msg_DateGet);
+    app.post('/stat/top_msg_title', stat.Top_msg_per_title);
 
-
+    
 }
