602c07d35177474549b71298032010fc10505bc3
mohamedbougarn <mohamedbougarn@gmail.com>
2022-05-04 10:20:17 +0100

    route & controller core for graph statisrique

--- controllers/stat.js
+++ controllers/stat.js
@@ -50,9 +50,34 @@ const Top_msg_per_title =(request, response) => {
 };
 
 
+//execute after the stat.js get count message per title  
+const Top_msg_per_annee =(request, response) => {
+
+    p_id_client = request.body.id_client;
+    p_top = request.body.top;
+    //p_response = request.body.response;
+
+    db.sequelize.query('SELECT * FROM ctl_top_countmsg_per_annee_select(:id_client, :top)',
+
+        { replacements: {id_client:p_id_client,top:p_top}, type: db.sequelize.QueryTypes.SELECT },
+        {
+            model: stat,
+            mapToModel: true // pass true here if you have any mapped fields
+        }).then(stat => {
+        logger.info(stat)
+        response.json(stat)
+    }).catch(err => {
+
+        logger.error(err)
+        response.status(500).json({msg: "error", details: err});
+    });
+
+};
+
 
 
 module.exports = {
     Stat_msg_DateGet,
-    Top_msg_per_title
+    Top_msg_per_title,
+    Top_msg_per_annee
 }
