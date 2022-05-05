4adc7923212a3a6b4b5589aaeadfa9c226b24387
mohamedbougarn <mohamedbougarn@gmail.com>
2022-05-05 15:02:33 +0100

    route & controller core for graph statisrique

--- routes/stat.js
+++ routes/stat.js
@@ -4,6 +4,7 @@ module.exports = function(app) {
     app.post('/stat/msg_date', stat.Stat_msg_DateGet);
     app.post('/stat/top_msg_title', stat.Top_msg_per_title);
     app.post('/stat/top_msg_annee', stat.Top_msg_per_annee);
+    app.post('/stat/top_msg_month', stat.Top_contextmsg_per_month);
 
     
 }
