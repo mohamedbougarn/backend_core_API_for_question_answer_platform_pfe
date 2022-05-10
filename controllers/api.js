4205a53043eabf9a62bc3e0f673a378f0d544d3b
mohamedbougarn <mohamedbougarn@gmail.com>
2022-05-10 23:10:13 +0100

    add method api_get in controller

--- controllers/api.js
+++ controllers/api.js
@@ -7,9 +7,9 @@ const api_Add =(request, response) => {
      p_id_client =request.body.id_client;
      p_key = request.body.key;
      
-    db.sequelize.query('SELECT * FROM ctl_langue_select(:id_langue) ',
+    db.sequelize.query('SELECT * FROM ctl_api_add(:id_client,:key) ',
 
-        { replacements: {id_langue:p_id_langue}, type: db.sequelize.QueryTypes.SELECT },
+        { replacements: {id_client:p_id_client,key:p_key}, type: db.sequelize.QueryTypes.SELECT },
         {
             model: api,
             mapToModel: true // pass true here if you have any mapped fields
@@ -25,6 +25,30 @@ const api_Add =(request, response) => {
 };
 
 
+const api_Get =(request, response) => {
+
+    p_id_client =request.body.id_client;
+    // p_key = request.body.key;
+    
+   db.sequelize.query('SELECT * FROM ctl_api_get(:id_client) ',
+
+       { replacements: {id_client:p_id_client}, type: db.sequelize.QueryTypes.SELECT },
+       {
+           model: api,
+           mapToModel: true // pass true here if you have any mapped fields
+       }).then(api => {
+       logger.info(api)
+       response.json(api)
+   }).catch(err => {
+
+       logger.error(err)
+       response.status(500).json({msg: "error", details: err});
+   });
+
+};
+
+
 module.exports = {
-    api_Add
+    api_Add,
+    api_Get
 }
