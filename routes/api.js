e80cf8625debba9dcfe2d5508b49cc0dca2471d2
mohamedbougarn <mohamedbougarn@gmail.com>
2022-05-14 10:55:49 +0100

    add routes

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,8 @@ module.exports = function(app) {
    
     app.put('/api/add',api.api_Add);
     app.put('/api/get',api.api_Get);
-    
+    app.put('/api/verif',api.api_verif_key);
+    app.put('/api/getcontext',api.getcontext);
 
 
 }
