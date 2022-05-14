d767c1e229fe197679cbc551dada5c4fe91e9f45
mohamedbougarn <mohamedbougarn@gmail.com>
2022-05-14 23:21:39 +0100

    update in routes

--- routes/treatment.js
+++ routes/treatment.js
@@ -2,6 +2,6 @@ module.exports = function(app) {
     const treatment = require('../controllers/treatment'); 
 
     app.post('/treatment/text', treatment.GetTextResponse);
-
+    app.post('/treatment/gpt3', treatment.GetGPT_3);
 
 }
