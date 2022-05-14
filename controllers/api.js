383992adb3f4cf1c4485a91578bbec7f29bba6db
mohamedbougarn <mohamedbougarn@gmail.com>
2022-05-14 10:55:28 +0100

    add method api_verif_key & getcontext  in controller

--- controllers/api.js
+++ controllers/api.js
@@ -1,6 +1,8 @@
 const  db=require('../config/db.config.js');
 const api = db.api;
+const context =db.api; 
 const logger = require('../config/logger');
+
 
 const api_Add =(request, response) => {
 
@@ -48,7 +50,56 @@ const api_Get =(request, response) => {
 };
 
 
+const api_verif_key = (request, response) => 
+{
+    
+    p_key = request.body.key;
+    
+   db.sequelize.query('SELECT * FROM ctl_verif_key(:key)',
+
+       { replacements: {key:p_key}, type: db.sequelize.QueryTypes.SELECT },
+       {
+           model: api,
+           mapToModel: true // pass true here if you have any mapped fields
+       }).then(api => {
+       logger.info(api)
+       response.json(api)
+   }).catch(err => {
+
+       logger.error(err)
+       response.status(500).json({msg: "error", details: err});
+   });
+
+};
+
+
+const getcontext =(request, response) => {
+
+    p_id_context =request.body.id_context;
+    db.sequelize.query('SELECT * FROM ctl_context_select(:id_context) ',
+
+        { replacements: {id_context:p_id_context}, type: db.sequelize.QueryTypes.SELECT },
+        {
+            model: context,
+            mapToModel: true // pass true here if you have any mapped fields
+        }).then(context => {
+        logger.info(context)
+        response.json(context)
+    }).catch(err => {
+
+        logger.error(err)
+        response.status(500).json({msg: "error", details: err});
+    });
+
+};
+
+
+
+
+
 module.exports = {
     api_Add,
-    api_Get
+    api_Get,
+    api_verif_key,
+    getcontext
 }
