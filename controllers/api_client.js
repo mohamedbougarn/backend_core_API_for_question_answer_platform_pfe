beabac56581472fb202240974babb5574e57e46d
mohamedbougarn <mohamedbougarn@gmail.com>
2022-05-14 10:56:51 +0100

    add  GeteResponseContext in api_client controller

---
+++ controllers/api_client.js
@@ -0,0 +1,53 @@
+const traitement = require('../controllers/treatment');
+const api = require('../controllers/api');
+const axios = require('axios')
+
+
+const URL = 'http://127.0.0.1:3000';
+
+const  GeteResponseContext= async (request, response ) => //, response 
+{
+
+    key = request.body.key;
+    id_context = request.body.id_context;
+    question = request.body.question;
+
+console.log(('00000000000000000000000000000'))
+           const veri_key = await  axios.put(URL+'/api/verif',{key});         
+
+           const  context_text  = await  axios.put(URL+'/api/getcontext',{id_context});        
+ console.log('1111111111111111111111111')
+
+         if(veri_key && veri_key.data && veri_key.data[0].ctl_verif_key == true && context_text && context_text.data &&context_text.data[0].text.length > 0)
+            
+         {
+           let context = context_text.data[0].text ;
+            result  = await axios.post(URL+'/treatment/text',{context,question});
+            //console.log(result)
+
+         }
+    
+console.log('2222222222222222222222222222222222222222222')
+    if(result && result.data )
+
+    {
+        var ddd = result.data;
+
+        console.log('dddddddddddddddddddddddd');
+        console.log(ddd)
+         response.send  (ddd)
+    }
+
+  else 
+
+  {
+      response.send (null)
+  }
+   
+
+};
+
+module.exports = {
+    GeteResponseContext
+}
+
