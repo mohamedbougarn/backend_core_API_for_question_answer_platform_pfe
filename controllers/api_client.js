907ef5b4adb49f6a8d816f0d7d169ebbbf5eeb81
mohamedbougarn <mohamedbougarn@gmail.com>
2022-05-17 18:17:20 +0100

    update test in api_client controller

--- controllers/api_client.js
+++ controllers/api_client.js
@@ -12,6 +12,7 @@ const  GeteResponseContext= async (request, response ) => //, response
     id_context = request.body.id_context;
     question = request.body.question;
 
+
 console.log(('00000000000000000000000000000'))
            const veri_key = await  axios.put(URL+'/api/verif',{key});         
 
@@ -25,6 +26,25 @@ console.log(('00000000000000000000000000000'))
            let context = context_text.data[0].text ;
             result  = await axios.post(URL+'/treatment/text',{context,question});
             //console.log(result)
+            console.log('2222222222222222222222222222222222222222222')
+            if(result && result.data )
+
+            {
+                var ddd = result.data;
+
+                console.log('dddddddddddddddddddddddd');
+                console.log(ddd)
+                response.send  (ddd)
+            }
+
+            else 
+
+            {
+                //response.send (null)
+                response.status(500).json({msg: "error", API_KEY: veri_key.data[0].ctl_verif_key});
+
+            }
+   
 
          }
          else
@@ -36,23 +56,7 @@ console.log(('00000000000000000000000000000'))
 
          }
     
-console.log('2222222222222222222222222222222222222222222')
-    if(result && result.data )
 
-    {
-        var ddd = result.data;
-
-        console.log('dddddddddddddddddddddddd');
-        console.log(ddd)
-         response.send  (ddd)
-    }
-
-  else 
-
-  {
-      response.send (null)
-  }
-   
 
 };
 
@@ -62,40 +66,60 @@ const  GeteResponseGPT_3 = async (request, response ) => //, response
 
     key = request.body.key;
     question = request.body.question;
+    lang=request.body.lang;
 
     console.log(('verif key'))
-           const veri_key = await  axios.put(URL+'/api/verif',{key});         
+           const veri_key = await  axios.put(URL+'/api/verif1',{key});         
     
-    console.log('verif key ok ')
+    console.log('verif key ok ',veri_key.data)
 
-         if(veri_key && veri_key.data && veri_key.data[0].ctl_verif_key == true)
-            
+         if(veri_key && veri_key.data && veri_key.data[0].ok == true) 
          {
-           let context = context_text.data[0].text ;
-            result  = await axios.post(URL+'/treatment/text',{context,question});
+           //let context = context_text.data[0].text ;
+            result  = await axios.post(URL+'/treatment/gpt3',{question});
             //console.log(result)
+
+            console.log('2222222222222222222222222222222222222222222')
+            if(result && result.data )
+
+            {
+                var ddd = result.data;
+
+                console.log('dddddddddddddddddddddddd');
+                console.log(ddd)
+                response.send(ddd)
+            }
+
+            else 
+
+            {
+                response.status(500).json({msg: "error", API_KEY: veri_key.data[0].ok});
+                //response.send (null)
+            }
+        
+
+         }
+         else
+         {
+            console.log('verif API KEY ', veri_key.data);
+           // response.send('verif API KEY = '+veri_key.data[0].ctl_verif_key);
+           //response.JSON(veri_key.data[0].ctl_verif_key);
+           response.status(500).json({msg: "error", API_KEY: veri_key.data[0].ok});
 
          }
     
-console.log('2222222222222222222222222222222222222222222')
-    if(result && result.data )
 
-    {
-        var ddd = result.data;
-
-        console.log('dddddddddddddddddddddddd');
-        console.log(ddd)
-         response.send(ddd)
-    }
-
-  else 
-
-  {
-      response.send (null)
-  }
-   
 
 };
+
+
+
+
+
+
+
+
+
 
 module.exports = {
     GeteResponseContext,
