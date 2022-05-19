aac3e14548b8cea6a626d5a0ea51ecaad9d2d75c
mohamedbougarn <mohamedbougarn@gmail.com>
2022-05-19 20:45:59 +0100

    node core backend

--- routes/context_conversation.js
+++ routes/context_conversation.js
@@ -6,6 +6,6 @@ module.exports = function(app) {
     app.put('/Context_conversation/add', contextconversation.Context_conversationAdd);
     app.put('/context_conversation/upload', contextconversation.Context_conversationUpload);
     app.put('/context_conversation/count', contextconversation.Context_ConversationCount);
-
+    app.put('/context_conversation/api/add',contextconversation.Context_conversationforApiAdd)
 
 }
